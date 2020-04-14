49e99a9dc2a134360628c1986d8071f3811b2a93
ArturVargas <arturvargas9226@gmail.com>
2020-04-13 21:39:34 -0500

    add react router and use withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 import './App.css';
 
 import Navbar from './components/Navbar/Navbar';
-import Card from './components/Card/Card';
 
 import Home from './containers/Home/Home';
+
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+);
 
 function App() {
   return (
     <div>
-     <Navbar />
-     <Home />
+      <Navbar />
+      <Switch>
+        <Route exact path='/' component={Home} />
+        <Route path='/hats' component={HatsPage} />
+      </Switch>
     </div>
   );
 }
