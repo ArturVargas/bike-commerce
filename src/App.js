356a31b094ceb309b21fc073dfea16ab75dc1020
ArturVargas <arturvargas9226@gmail.com>
2020-10-19 21:50:59 -0500

    redux progress

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import './App.css';
 
@@ -17,7 +17,7 @@ class App extends Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    const {setCurrentUser} = this.props;
+    const { setCurrentUser } = this.props;
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
       if(userAuth) {
         const userRef = await createUserProfileDoc(userAuth);
@@ -43,16 +43,28 @@ class App extends Component {
         <Navbar/>
         <Switch>
           <Route exact path='/' component={Home} />
-          <Route path='/login' component={Login} />
           <Route path='/shop' component={Shop} />
+          <Route exact path='/login' render={() => 
+            this.props.currentUser ? 
+            (<Redirect to='/' />)
+            : (<Login />)
+          }
+          />
         </Switch>
       </div>
     );
   }
 }
 
+const mapStateToProps = ({ user }) => ({
+  currentUser: user.currentUser
+});
+
 const mapDispatchToProps = (dispatch) => ({
   setCurrentUser: (user) => dispatch(setCurrentUser(user))
 });
 
-export default connect(null, mapDispatchToProps)(App);
+export default connect(
+  mapStateToProps, 
+  mapDispatchToProps
+)(App);
