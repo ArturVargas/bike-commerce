b58944fb1b52ec099b10a861ed8e7fbfc1b29a19
ArturVargas <arturvargas9226@gmail.com>
2020-10-22 23:16:33 -0500

    create selectors using reselect

--- src/components/CartIcon/CartIcon.js
+++ src/components/CartIcon/CartIcon.js
@@ -2,15 +2,16 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 import { setDropdownVisible } from '../../redux/cart/cart.actions';
+import { selectCartQuantityItems } from '../../redux/cart/cart.selectors';
 
 import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
 
 import './CartIcon.scss';
 
-const CartIcon = ({ setDropdownVisible }) => (
+const CartIcon = ({ setDropdownVisible, itemCount }) => (
   <div className="cart-icon" onClick={setDropdownVisible}>
     <ShoppingIcon className="shopping-icon" />
-    <span className="item-count">0</span>
+    <span className="item-count">{itemCount}</span>
   </div>
 );
 
@@ -18,7 +19,11 @@ const mapDispatchToProps = (dispatch) => ({
   setDropdownVisible: () => dispatch(setDropdownVisible())
 });
 
+const mapStateToProps = (state) => ({
+  itemCount: selectCartQuantityItems(state)
+});
+
 export default connect(
-  null,
+  mapStateToProps,
   mapDispatchToProps
 )(CartIcon);
